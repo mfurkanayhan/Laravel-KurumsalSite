f3d68ddd32b84c55fe64b3fa076ef46bcd4154af
mfurkanayhan <mfurkana067@gmail.com>
2021-01-07 19:21:42 +0300

    Laravel Routing Controller View Kullanımı

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>First Laravel Page</title>
+</head>
+<body>
+
+<h1>Welcome To Laravel</h1>
+<p>This is a paragraph.</p>
+<br>
+<a href="{{route('test', ['id' => 12, 'name' => 'Fatma'])}}">Test Sayfası</a>
+</body>
+</html>
